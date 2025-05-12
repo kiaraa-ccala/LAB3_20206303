e4e0463619a2954e9822e51bafe14f483a06d84f
kiaraa-ccala <kiaraa.ccala@pucp.edu.pe>
2025-05-12 11:13:37 -0500

    Restauro archivos y completo hasta P4

---
+++ src/main/java/demo/lab3_20206303/Lab320206303Application.java
@@ -0,0 +1,13 @@
+package demo.lab3_20206303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320206303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320206303Application.class, args);
+    }
+
+}
